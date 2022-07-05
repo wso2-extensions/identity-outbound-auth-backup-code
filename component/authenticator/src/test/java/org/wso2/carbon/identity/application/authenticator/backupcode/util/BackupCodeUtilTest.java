1ea15c7e92ae6ca37cd8060e804c0a224ff3ecc2
lashinijay <lashinisharikaj@gmail.com>
2022-07-05 16:38:23 +0530

    adding unit tests

--- component/authenticator/src/test/java/org/wso2/carbon/identity/application/authenticator/backupcode/util/BackupCodeUtilTest.java
+++ component/authenticator/src/test/java/org/wso2/carbon/identity/application/authenticator/backupcode/util/BackupCodeUtilTest.java
@@ -1,8 +1,10 @@
 package org.wso2.carbon.identity.application.authenticator.backupcode.util;
 
 import junit.framework.TestCase;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.application.authentication.framework.config.model.SequenceConfig;
@@ -10,14 +12,30 @@ import org.wso2.carbon.identity.application.authentication.framework.context.Aut
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.application.authenticator.backupcode.exception.BackupCodeException;
 import org.wso2.carbon.identity.application.authenticator.backupcode.internal.BackupCodeDataHolder;
+import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
+import org.wso2.carbon.user.api.UserRealm;
+import org.wso2.carbon.user.api.UserStoreException;
+import org.wso2.carbon.user.core.service.RealmService;
+import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
-@PrepareForTest({BackupCodeUtil.class, BackupCodeDataHolder.class})
-public class BackupCodeUtilTest extends TestCase {
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.testng.AssertJUnit.assertEquals;
+
+@PrepareForTest({BackupCodeUtil.class, BackupCodeDataHolder.class, MultitenantUtils.class, IdentityTenantUtil.class})
+public class BackupCodeUtilTest extends PowerMockTestCase {
+
+    private String username = "test1";
+    private String tenantDomain = "test.domain";
+    private int tenantId = -1234;
 
     BackupCodeUtil backupCodeUtil = new BackupCodeUtil();
 
-    @Spy
-    AuthenticationContext authenticationContext;
+    @Mock
+    RealmService realmService;
+
+    @Mock
+    UserRealm userRealm;
 
     @Test(dataProvider = "hashStringData")
     public void testGenerateHashString(String backupCode) throws BackupCodeException {
@@ -41,19 +59,30 @@ public class BackupCodeUtilTest extends TestCase {
 
     @Test
     public void testGetAuthenticatedUser() {
-        AuthenticationContext authenticationContext1 = new AuthenticationContext();
+
+        AuthenticationContext authenticationContext = new AuthenticationContext();
         SequenceConfig sequenceConfig = new SequenceConfig();
-        authenticationContext1.setSequenceConfig(sequenceConfig);
-        AuthenticatedUser authenticatedUser = backupCodeUtil.getAuthenticatedUser(authenticationContext1);
+        authenticationContext.setSequenceConfig(sequenceConfig);
+        AuthenticatedUser authenticatedUser = backupCodeUtil.getAuthenticatedUser(authenticationContext);
         assertEquals(null, authenticatedUser);
     }
 
     @Test
     public void testGetRealmService() {
+
+
     }
 
     @Test
-    public void testGetUserRealm() {
+    public void testGetUserRealm() throws UserStoreException {
+        mockStatic(MultitenantUtils.class);
+        mockStatic(IdentityTenantUtil.class);
+        mockStatic(BackupCodeDataHolder.class);
+        when(MultitenantUtils.getTenantDomain(username)).thenReturn(tenantDomain);
+        when(IdentityTenantUtil.getTenantId(tenantDomain)).thenReturn(tenantId);
+        when(BackupCodeDataHolder.getRealmService()).thenReturn(realmService);
+        when(realmService.getTenantUserRealm(tenantId)).thenReturn(userRealm);
+        assertEquals(userRealm, backupCodeUtil.getUserRealm(username));
     }
 
     @Test
